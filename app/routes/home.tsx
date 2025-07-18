3b22497415b9a2439c76af853cf07863638bffb2
ritikjain07 <ritikjain4560@gmail.com>
2025-07-19 00:38:53 +0530

    home page completed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,38 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants/index";
+import Navbar from "~/components/Navbar";
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "HireMe AI" },
+    { name: "description", content: "Get Noticed. Get Hired. Smarter with AI." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application & Resume Rating</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+
+      {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+      <div key={resume.id}>
+        <ResumeCard key={resume.id} resume={resume} />
+      </div>
+    ))}
+      </div>
+    )}
+    </section>
+
+
+    
+
+    
+  </main>
 }
